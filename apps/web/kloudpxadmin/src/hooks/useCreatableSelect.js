50359834be304c81e8cbda63eeb0ae532de81882
RinkiMandal <rinkimandal944@gmail.com>
2025-06-24 12:07:14 +0530

    generic, supplier, catgory id error fixed

--- apps/web/kloudpxadmin/src/hooks/useCreatableSelect.js
+++ apps/web/kloudpxadmin/src/hooks/useCreatableSelect.js
@@ -1,13 +1,17 @@
 import { useState } from "react";
 
-const useCreatableSelect = (initialValue = null) => {
+const useCreatableSelect = (createFn, initialValue = null) => {
   const [value, setValue] = useState(initialValue);
 
   const handleChange = (selectedOption) => {
     setValue(selectedOption);
   };
 
-  const handleCreateOption = async (inputValue, createFn) => {
+  const handleCreateOption = async (inputValue) => {
+    if (!createFn) {
+      console.warn("No create function provided");
+      return null;
+    }
     const newOption = await createFn(inputValue);
     if (newOption) {
       setValue(newOption);
