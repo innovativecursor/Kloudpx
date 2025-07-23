c0bb0e9bb2a2b361bfa3d5d6b2f68041f9156ada
vaish567 <vaishnav.parte@gmail.com>
2025-07-23 17:26:15 +0530

    update on vaishnav

--- apps/config/env.js
+++ apps/config/env.js
@@ -1,9 +1,11 @@
 const isLive = process.env.NODE_ENV === "production";
 
 const baseUrls = {
-  admin: isLive ? "https://api.admin.kloudpx.com" : "http://localhost:10001",
+  admin: isLive
+    ? process.env.NEXT_PUBLIC_ADMIN_BASE_URL
+    : "http://localhost:10001",
   pharmacist: isLive
-    ? "https://api.pharmacist.kloudpx.com"
+    ? process.env.NEXT_PUBLIC_PHARMACIST_BASE_URL
     : "http://localhost:10002",
 };
 
