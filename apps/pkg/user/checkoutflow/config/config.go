c531e37253f1a5fd5fb2d7f2bdb9d744ceb6b565
KiranPawar0 <kiran.pawar@bharatverified.in>
2025-07-29 10:03:00 +0530

    checkout flow changes

--- apps/pkg/user/checkoutflow/config/config.go
+++ apps/pkg/user/checkoutflow/config/config.go
@@ -5,6 +5,7 @@ type AddressRequest struct {
 	NameResidency string `json:"nameresidency"`
 	Region        string `json:"region"`
 	Province      string `json:"province"`
+	Barangay      string `json:"barangay"`
 	City          string `json:"city"`
 	ZipCode       string `json:"zipcode"`
 	IsDefault     bool   `json:"isdefault"`
@@ -18,4 +19,40 @@ type ReqDelivery struct {
 
 type SelectAddress struct {
 	AddressID uint `json:"addressid"`
+}
+
+type SubmitPaymentRequest struct {
+	CheckoutSessionID string `json:"checkout_session_id"`
+	PaymentNumber     string `json:"payment_number"`
+	AmountPaid        string `json:"amount_paid"`
+	ScreenshotBase64  string `json:"screenshot_base64"`
+}
+type UserFacingMedicine struct {
+	ID                        uint     `json:"id"`
+	BrandName                 string   `json:"brand_name"`
+	Power                     string   `json:"power"`
+	GenericName               string   `json:"generic_name"`
+	Discount                  string   `json:"discount"`
+	Category                  string   `json:"category"`
+	Description               string   `json:"description"`
+	Unit                      string   `json:"unit"`
+	MeasurementUnitValue      int      `json:"measurement_unit_value"`
+	NumberOfPiecesPerBox      int      `json:"number_of_pieces_per_box"`
+	Price                     float64  `json:"price"`
+	TaxType                   string   `json:"tax_type"`
+	Prescription              bool     `json:"prescription"`
+	Benefits                  string   `json:"benefits"`
+	KeyIngredients            string   `json:"key_ingredients"`
+	RecommendedDailyAllowance string   `json:"recommended_daily_allowance"`
+	DirectionsForUse          string   `json:"directions_for_use"`
+	SafetyInformation         string   `json:"safety_information"`
+	Storage                   string   `json:"storage"`
+	Images                    []string `json:"images"`
+}
+
+type CartResponse struct {
+	CartID             uint               `json:"cart_id"`
+	Quantity           int                `json:"quantity"`
+	Medicine           UserFacingMedicine `json:"medicine"`
+	PrescriptionStatus string             `json:"prescription_status"`
 }
