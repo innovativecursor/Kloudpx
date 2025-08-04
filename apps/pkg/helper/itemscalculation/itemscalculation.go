35e485a7a22a3edb11a620584491d5e7591f4af9
KiranPawar0 <kiran.pawar@bharatverified.in>
2025-08-04 15:19:50 +0530

    all issues sloved

--- apps/pkg/helper/itemscalculation/itemscalculation.go
+++ apps/pkg/helper/itemscalculation/itemscalculation.go
@@ -5,20 +5,17 @@ import (
 	"gorm.io/gorm"
 )
 
-// CalculateTotalStockByBrandName calculates the total available stock for a given brand, summing across suppliers
-func CalculateTotalStockByBrandName(db *gorm.DB, brandName string) (int, error) {
+func CalculateTotalStockByBrandName(db *gorm.DB, brandName, power string) (int, error) {
 	var medicines []models.Medicine
-	if err := db.Where("brand_name = ?", brandName).Find(&medicines).Error; err != nil {
+	if err := db.Where("brand_name = ? AND power = ?", brandName, power).Find(&medicines).Error; err != nil {
 		return 0, err
 	}
 
 	total := 0
 	for _, m := range medicines {
 		if m.UnitOfMeasurement == "per box" {
-			// Total pieces = boxes * pieces per box
 			total += m.MeasurementUnitValue * m.NumberOfPiecesPerBox
 		} else if m.UnitOfMeasurement == "per piece" {
-			// Just add the number of pieces directly
 			total += m.MeasurementUnitValue
 		}
 	}
