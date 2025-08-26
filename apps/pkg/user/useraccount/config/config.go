2150c919e83d0130ff04cf38c203a4e57014912b
Aanchal-dev8202 <sharmaanchal8202@gmail.com>
2025-08-26 15:20:48 +0530

    Develop API to upload and update the PWD certificate

--- apps/pkg/user/useraccount/config/config.go
+++ apps/pkg/user/useraccount/config/config.go
@@ -6,3 +6,7 @@ type UpdateProfileInput struct {
 	DOB      string  `json:"dob" binding:"required"` // frontend sends DOB as YYYY-MM-DD
 	Gender   string  `json:"gender" binding:"required,oneof=Male Female Other"`
 }
+
+type PwdPayload struct {
+	File string `json:"file" binding:"required"` // base64-encoded certificate
+}
