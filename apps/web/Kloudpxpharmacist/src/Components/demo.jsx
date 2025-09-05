e713dc6681dc1909ba6bf9b46d059f415a116a08
RinkiMandal <rinkimandal944@gmail.com>
2025-09-05 20:22:19 +0530

    all done in fe

--- apps/web/Kloudpxpharmacist/src/Components/demo.jsx
+++ apps/web/Kloudpxpharmacist/src/Components/demo.jsx
@@ -1,24 +1,11 @@
-  const handleSaveAndProceed = () => {
-    if (!checkoutData?.items || checkoutData.items.length === 0) {
-      toast.error("No items in checkout. Add items first!");
-      return;
-    }
+import React from 'react'
 
-    let selectedAddressId = selectedId;
+const demo = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
 
-    if (!selectedAddressId) {
-      const defaultAddress = addresses.find((addr) => addr.IsDefault === true);
-      if (defaultAddress) {
-        selectedAddressId = defaultAddress.ID;
-      }
-    }
-
-    if (selectedAddressId) {
-      startLoader();
-      selectedAddress(selectedAddressId);
-      setSelectedId(selectedAddressId);
-      router.push("Delivery");
-    } else {
-      toast.error("Please select an address");
-    }
-  };
+export default demo
